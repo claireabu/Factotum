5046fd4957c34db29624690f5cd70b1ffa7e17dd
Claire <claire.abuhakima@gmail.com>
2011-02-09 22:22:44 -0800

    ling data and attributes added

---
+++ lingdata.f
@@ -0,0 +1,93 @@
+:English-> English. 
+:" <Indo-European>.
+:" <Germanic>.
+" is written using the Latin alphabet.  
+:French-> Français.
+:" <Indo-European>.
+:" <Italic>. 
+" is written using the Latin alphabet. 
+:Italian-> Italiano.
+:" <Indo-European>.
+:" <Italic>.
+" is written using the Latin alphabet.
+:Spanish-> Español.
+:" -> Castellano. 
+:" <Indo-European>.
+:" <Italic>.
+" is written using the Latin alphabet.
+:German-> Deutsch. 
+:" <Indo-European>.
+:" <Germanic>.
+" is written using the Latin alphabet. 
+:Finnish-> Suomi. 
+:" -> Suomen kieli. 
+:" <Uralic>.
+:" <Finno-Ugric>. 
+" is written using the Latin alphabet. 
+:Serbian -> Srpski. 
+:" -> српски. 
+:" <Indo-European>. 
+:" <Balto-Slavic>. 
+" is written using the Latin alphabet. 
+" is written using the Cyrillic alphabet.  
+:Swedish-> Svenska. 
+:" <Indo-European>.
+:" <Germanic>.
+" is written using the Latin alphabet. 
+:Arabic-> al-ʿarabiyyah.
+:" -> العربية.
+:" <Afro-Asiatic>.
+:" <Semitic>. 
+" is written using the Arabic alphabet. 
+" is written using the Syriac alphabet.
+" has only three vowels and two dipthongs. 
+" uses VSO word order, though is flexible.   
+:Farsi-> Persian. 
+:" < Indo-European>. 
+:" <Indo-Iranian>.
+" is written using Perso-Arabic script. 
+:Dari-> Dari(Persian). 
+:" <Indo-European>.
+:" <Indo-Iranian>. 
+:Pashto-> Pakhto. 
+:" -> Pushto. 
+:" -> Pukhto.
+:" -> Pashtu.
+:" -> Pushtu. 
+:" -> Afghani. 
+:" <Indo-European>. 
+:" <Indo-Iranian>. 
+" is written using the Pashto alphabet. 
+:Russian->  русский язык.
+:" -> Russkiy yazyk. 
+:" <Indo-European>. 
+:" <Balto-Slavic>. 
+" is written using the Cyrillic alphabet. 
+:Mandarin-> Mandarin Chinese. 
+:" ->  官話.
+:" <Sino-Tibetan>.
+:" <Sinitic>.  
+:Cantonese-> Cantonese Chinese. 
+:" -> Yue. 
+:" <Sino-Tibetan>. 
+:" <Sinitic>. 
+:Japanese-> Nihongo. 
+:" -> 日本語.
+:" <Japonic>. 
+" uses SOV word order. 
+" has only pure vowels, no dipthongs. 
+:Hindi-> Modern Standard Hindi. 
+:" -> Manak Hindi.
+:" <Indo-European>.
+:" <Indo-Iranian>.
+:Taa -> ǃXóõ.
+:" <Tuu>.
+:" <Taa-Lower Nossop>.  
+" is famous for its abundance of click sounds and clusters. 
+:Greek-> ελληνικά.
+:" <Indo-European>.
+:" <Hellenic>. 
+:Latin-> latīna. 
+:" <Indo-European>. 
+:" <Italic>. 
+" is a dead language and no longer spoken.  
