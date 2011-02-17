b7eea5fb542b511a413c3785611c818e0c77dd57
Claire <claire.abuhakima@gmail.com>
2011-02-17 10:39:44 -0800

    ling data thurs 17

---
+++ lingdata.f
@@ -0,0 +1,117 @@
+:English-> English. 
+:" <Indo-European>.
+:" <Germanic>.
+" is written using the Latin alphabet.  
+" <SVO> word order. 
+:French-> Français.
+:" <Indo-European>.
+:" <Italic>. 
+" is written using the Latin alphabet -f.
+" <SVO> word order.  
+:Italian-> Italiano.
+:" <Indo-European>.
+:" <Italic>.
+" is written using the Latin alphabet -i.
+" <SVO> word order. 
+:Spanish-> Español.
+:" -> Castellano. 
+:" <Indo-European>.
+:" <Italic>.
+" is written using the Latin alphabet -s.
+" <SVO> word order. 
+:German-> Deutsch. 
+:" <Indo-European>.
+:" <Germanic>.
+" is written using the Latin alphabet. 
+" <SVO> word order -g. 
+:Finnish-> Suomi. 
+:" -> Suomen kieli. 
+:" <Uralic>.
+:" <Finno-Ugric>. 
+" is written using the Latin alphabet. 
+" no strict word order
+- sentence structure flexible
+- <SVO> most frequent word order. 
+:Serbian -> Srpski. 
+:" -> српски. 
+:" <Indo-European>. 
+:" <Balto-Slavic>. 
+" is written using the Latin alphabet -ser. 
+" is written using the Cyrillic alphabet.
+" <SVO> word order.   
+:Swedish-> Svenska. 
+:" <Indo-European>.
+:" <Germanic>.
+" is written using the Latin alphabet.
+" <SVO> word order.  
+:Arabic-> al-ʿarabiyyah.
+:" -> العربية.
+:" <Afro-Asiatic>.
+:" <Semitic>. 
+" is written using the Arabic alphabet. 
+" is written using the Syriac alphabet.
+" <VSO> word order. 
+" has only three vowels and two dipthongs. 
+:Farsi-> Persian. 
+:" < Indo-European>. 
+:" <Indo-Iranian>.
+" is written using Perso-Arabic script. 
+" <SOV> word order. 
+:Dari-> Dari(Persian). 
+:" <Indo-European>.
+:" <Indo-Iranian>. 
+" <SOV> word order. 
+:Pashto-> Pakhto. 
+:" -> Pushto. 
+:" -> Pukhto.
+:" -> Pashtu.
+:" -> Pushtu. 
+:" -> Afghani. 
+:" <Indo-European>. 
+:" <Indo-Iranian>. 
+" is written using the Pashto alphabet. 
+" <SOV> word order. 
+:Russian->  русский язык.
+:" -> Russkiy yazyk. 
+:" <Indo-European>. 
+:" <Balto-Slavic>. 
+" is written using the Cyrillic alphabet. 
+" <SVO> most frequent word order 
+- allows all possible combinations
+- influences the nuance of the meaning.   
+:Mandarin-> Mandarin Chinese. 
+:" ->  官話.
+:" <Sino-Tibetan>.
+:" <Sinitic>. 
+" <SVO> word order
+- but word order flexible.  
+:Cantonese-> Cantonese Chinese. 
+:" -> Yue. 
+:" <Sino-Tibetan>. 
+:" <Sinitic>. 
+:Japanese-> Nihongo. 
+:" -> 日本語.
+:" <Japonic>. 
+" <SOV> word order.  
+" has only pure vowels, no dipthongs. 
+:Hindi-> Modern Standard Hindi. 
+:" -> Manak Hindi.
+:" <Indo-European>.
+:" <Indo-Iranian>.
+" <SOV> word order.
+:Taa -> ǃXóõ.
+:" -> !Xoon
+:" <Tuu>.
+:" <Taa-Lower Nossop>.  
+" <SVO> word order. 
+" is famous for its abundance of click sounds and clusters. 
+:Greek-> ελληνικά.
+:" <Indo-European>.
+:" <Hellenic>. 
+" <SVO> word order. 
+:Latin-> latīna. 
+:" <Indo-European>. 
+:" <Italic>. 
+" no strict word order
+- sentence structure highly flexible
+- <SOV> is most frequent # outside of poetry. 
