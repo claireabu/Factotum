f329a1f0dbcab2ebc4097b21fd42eef809fd6096
Claire <claire.abuhakima@gmail.com>
2011-02-25 15:20:46 -0800

    lingdata update

--- lingdata.f
+++ lingdata.f
@@ -1,29 +1,284 @@
-:English-> English. 
-:" <Indo-European>.
-:" <Germanic>.
-" is written using the Latin alphabet.  
-" <SVO> word order. 
 :French-> Français.
 :" <Indo-European>.
 :" <Italic>. 
-" is written using the Latin alphabet -f.
-" <SVO> word order.  
+" is written using the Latin alphabet -f. #-f: 4 diacritics, a cedilla, and two ligatures
+" <SVO> word order. 
+" is an official language of Belgium. 
+" is an official language of Benin.
+" is an official language of Burkina Faso.
+" is an official language of Burundi. 
+" is an official language of Cameroon. 
+" is an official language of Central African Republic. 
+" is an official language of Chad. 
+" is an official language of Comoros. 
+" is an official language of Cote d'Ivoire. 
+" is an official language of Democratic Republic of the Congo.
+" is an official language of Dijbouti. 
+" is an official language of Equitorial Guinea. 
+" is an official language of France. 
+" is an official language of Gabon. 
+" is an official language of Guinea. 
+" is an official language of Haiti. 
+" is an official language of Lebanon. 
+" is an official language of Luxembourg. 
+" is an official language of Madagascar. 
+" is an official language of Mali. 
+" is an official language of Monaco. 
+" is an official language of Niger. 
+" is an official language of Republic of the Congo. 
+" is an official language of Rwanda. 
+" is an official language of Senegal. 
+" is an official language of Seychelles. 
+" is an official language of Switzerland. 
+" is an official language of Togo. 
+" is an official language of Vanuatu. 
+" is an official language of Saarland. 
+" is an official language of Aosta Valley.
+" is an official language of Clipperton. 
+" is an official language of French Polynesia. 
+" is an official language of Guernsey. 
+" is an official language of Jersey. 
+" is an official language of Mayotte. 
+" is an official language of New Caledonia. 
+" is an official language of Pudcherry. 
+" is an official language of Saint-Barthelemy. 
+" is an official language of Saint-Martin. 
+" is an official language of Saint-Pierre and Miquelon. 
+" is an official language of Wallis and Futuna. 
+" is one of the languages of the United Nations. 
+" inflected: yes, moderately. 
+" negation is expressed in two parts. 
+" articles agree in gender and number with the noun. 
+" determiners agree in gender and number with the noun. 
+" adjectives agree in gender and number with the noun. 
+" adverbs are invariable. 
+" verbs reflect: mood: indicative. 
+" verbs reflect: mood: imperative. 
+" verbs reflect: mood: subjunctive. 
+" verbs reflect: mood: conditional. 
+" verbs reflect: mood: infinitive. 
+" verbs reflect: mood: participle. 
+" verbs reflect: mood: gerundive. 
+" verbs reflect: tense: present. 
+" verbs reflect: tense: imperfect. 
+" verbs reflect: tense: future. 
+" verbs reflect: tense: conditional. 
+" verbs reflect: tense: preterite (simple past). 
+" verbs reflect: aspect: perfect. 
+" verbs reflect: aspect: not perfect. 
+" verbs reflect: voice: active.
+" verbs reflect: voice: passive. 
+" verbs reflect: voice: reflexive. 
+" uses the Latin alphabet -f. 
+# -f: the addition of 4 diacritics, the accute accent, grave accent, circumflex, diaeresis, and also a cedilla (ç).
+" grammatical genders: two, masculine and feminine. 
+" null-subject/pro-drop language: no.
+" IPA consonants: m ŋ ɲ n p b t d k ɡ f v s z ʃ ʒ j ɥ w ʁ l (ʎ).
+" IPA vowels:  i y e ø ə u o ɔ ɛ̃ (œ̃) ɔ̃ ɑ̃ a (ɑ)
+
+     
 :Italian-> Italiano.
 :" <Indo-European>.
 :" <Italic>.
-" is written using the Latin alphabet -i.
+" is an official language of Italy. 
+" is an official language of Switzerland. 
+" is an official language of San Marino. 
+" is an official language of Vatican City. 
+" is an official language of Soverign Military Order of Malta. 
+" is an official language of Croatia- Istria County. 
+" is an official language of Slovenia- Slovenian Istria. 
+" dialect: Tuscan. 
+" dialect: Central Italian. 
+" dialect: Corsican. 
+# note that there is a distinct difference between Italian dialects and languages of Italy. These three listed dialects are those closest to Italian structure. 
+" is written using the Latin alphabet -i. #-i, Italian uses the Latin alphabet but only has 21 letters: it excludes j, k, w, x, y.  Also, it has 3 diacritics, the accute accent, grave accent, and the circumflex. 
 " <SVO> word order. 
+" grammatical gender: two (masculin, feminine). 
+#note: some words may change gender depending on number. 
+" nouns inflect by gender. 
+" nouns inflect by number. 
+" adjectives inflect by gender. 
+" adjectives inflect by number. 
+" diminuitives can alter meaning of noun. 
+" augmentatives can alter meaning of noun. 
+" pejoratives can alter meaning of noun.
+" null subject/pro-drop: yes. #conjugation is enough to determine grammatical person. Pronoun used mainly when emphasis on subject is needed. 
+" verbs reflect: tense: present. 
+" verbs reflect: tense: imperfect. 
+" verbs reflect: tense: future. 
+" verbs reflect: tense: preterite. 
+" verbs reflect: mood: indicative. 
+" verbs reflect: mood: conditional. 
+" verbs reflect: mood: subjunctive. 
+" verbs reflect: mood: imperative. 
+" verbs reflect: mood: infinitive. 
+" verbs reflect: mood: gerund. 
+" verbs reflect: mood: participle. 
+" verbs reflect: aspect: no. #Italian grammar does not explicitly recognize verbal aspect, though it may be rendered through the use of different tenses and periphrases. 
+" IPA vowels: i u e o ɛ ɔ a. 
+" IPA consonants: m n ɲ p b t d k ɡ ʦ ʣ ʧ ʤ f v s z ʃ r l ʎ j w. 
+
+:German-> Deutsch. 
+:" <Indo-European>.
+:" <Germanic>.
+" is an official language of Austria. 
+" is an official language of Belgium. 
+" is an official language of Italy- province of Bolzano-Bozen.
+" is an official language of Germany.
+" is an official language of Liechentenstein. 
+" is an official language of Luxembourg. 
+" is an official language of Switzerland. 
+" standard German orginated as a written language (not as a dialect). 
+" standard German differs regionally. #vocabulary, pronunciation, orthography, and even grammar. 
+" pluricentric. 
+#standard German is pluricentric, meanting there are variations of standard German, which is not the saeme as variation of local dialects. German dialects are traditional local varieties that can be traced back to tribes. The variation between dialects is considerable, and mainly neighboring dialects are mutually intelligible.
+" dialect: High German. 
+" dialect: Low German. # did not participate in high German consonant shift (second consonant shift). 
+" grammatical genders: three (masculine, feminine, neuter).   
+" grammatical cases: four. 
+" nouns inflect for cases. 
+#note: all nouns are capitalized. 
+" case: nominative. 
+" case: genitive. 
+" case: dative.
+" case: accusative. 
+" conjucation classes: two (strong, weak). 
+" verbs inflect to: conjucation class: strong. 
+" verbs inflect to: conjucation class: weak. 
+" verbs inflect to: person: first (1st).
+" verbs inflect to: person: second (2nd). 
+" verbs inflect to: person: third (3rd). 
+" verbs inflect to: mood: indicative. 
+" verbs inflect to: mood: imperative. 
+" verbs inflect to: mood: subjunctive. 
+" verbs inflect to: voice: active.
+" verbs inflect to: voice: passive.
+" verbs inflect to: number: singular.
+" verbs inflect to: number: plural. 
+" verbs inflect to: aspect: perfect. 
+" verbs inflect to: aspect: progressive. 
+" verb prefixes can add meaning to the base verb. 
+" is written using the Latin alphabet -g. #-g, additional letters include three vowels with umlauts,  (ä, ö, ü)  and the "sharp s" ß.
+" IPA vowels: i iː y yː u uː ɪ ʏ ʊ e eː ø øː o oː ə ɛ ɛː œ  ɔ ɐ a aː . 
+" IPA diphthongs:  a͡ɪ a͡ʊ ɔ͡ʏ .
+" IPA consonants: p b t d k ɡ ʔ  p͡f  ʦ ʧ ʤ f v s z ʃ ʒ ç x ʁ h m n ŋ l j r ʀ. 
+" <SVO> word order -g. 
+
 :Spanish-> Español.
 :" -> Castellano. 
 :" <Indo-European>.
 :" <Italic>.
-" is written using the Latin alphabet -s.
+" is written using the Latin alphabet -s. #-s Latin alphabet with the addition of ñ and the diagraphs ch and ll. 
 " <SVO> word order. 
-:German-> Deutsch. 
+" is an official language of the United Nations. 
+" is an official language of Colombia. #by law
+" is an official language of Spain. #by law
+" is an official language of Peru. #by law
+" is an official language of Venezuela. #by law
+" is an official language of Ecuador. #by law
+" is an official language of Guatemala. #by law
+" is an official language of Cuba. #by law
+" is an official language of Bolivia. #by law
+" is an official language of Honduras. #by law
+" is an official language of El Salvador. #by law
+" is an official language of Paraguay. #by law
+" is an official language of Costa Rica. #by law
+" is an official language of Panama. #by law
+" is an official language of Equatorial Guinea. #by law 
+" is an official language of Puerto Rico. #by law
+" is an official language of Mexico. #de facto
+" is an official language of Argentina. #de facto. 
+" is an official language of Chile. #de facto. 
+" is an official language of Dominican Republic. #de facto
+" is an official language of Nicaragua. #de facto
+" is an official language of Uruguay. #de facto 
+" dialect: Castillian. #the standard in Spain. The difference from other dialects is that it has the voiceless dental fricative /θ/ that others lack. 
+" dialect: Andalusian. 
+" dialect: Central American. 
+" dialect: Latin American. 
+" dialect: Mexican Spanish. 
+# note: differences between peninsular (Spain) and American spanish are few, and there should be no trouble communication between dialects, i.e. mutually intelligible (though vocabulary and pronunciation varies).
+" verbs reflect: tense: present. 
+" verbs reflect: tense: preterite. 
+" verbs reflect: tense: imperfect. 
+" verbs reflect: tense: present perfect. 
+" verbs reflect: tense: past perfect. 
+" verbs reflect: tense: future. 
+" verbs reflect: tense: conditional.
+" verbs reflect: mood: indicative. 
+" verbs reflect: mood: subjunctive. 
+" verbs reflect: mood: imperative. 
+" verbs reflect: aspect: perfective. 
+" verbs reflect: aspect: imperfective. 
+" verbs reflect: voice: active. 
+" verbs reflect: voice: passive. 
+" verbs reflect: number. 
+" verbs reflect: person. 
+" null subject/pro-drop: yes.  #note: personal pronouns are rare in spoken language (save for emphasis), but not rare in written text.  
+" determiners agree in gender and number with what they refer to. 
+" grammatical genders: two (masculine and feminine) -x. # note that there is one neuter article "lo"
+" prepositions always occur in front of verb. #no "postprositions"
+" prepositions do not change meaning of the verb. 
+" inflected: yes. 
+" nouns inflect for gender and number. 
+" adjectives inflect for gender and number. 
+" determiners inflect for gender and number. 
+" IPA vowels:  i u e̝ o̝̞ ä . 
+" IPA consonants: m n ɲ p b t (θ) d s ʧ ʝ k x ɡ r ɾ l (ʎ) .
+" IPA diphthongs: /ei/ /ai/ /oi/ /eu/ /au/ /ou/ /je/ /ja/ /jo/ /ju/  /wi/ /we/ /wa/ /wo/ . 
+
+:Farsi-> Persian. 
+:" < Indo-European>. 
+:" <Indo-Iranian>.
+" is written using Perso-Arabic script. 
+" <SOV> word order. 
+# continuation of Middle Persian. 
+" pluricentric language. 
+" spoken in: Iran. 
+" spoken in: Afghanistan. 
+" spoken in: Tajikistan. 
+" spoken in: Uzbekistan. 
+" dialect: Dari. #local official name in Afghanistan, based on Persian Literature.
+" dialect: Farsi. #local name in Iran, based on Persian Literature. 
+" dialect: Tajiki. #local name in Central Asia (Tajikistan, Uzbekistan, Russia), based on Persian literature. 
+" dialect: Hazaragi. 
+" dialect: Aimaq. 
+" dialect: Bukharic. 
+" dialect: Dzhidi. 
+" dialect: Pahlavani.
+" dialect: Herati. 
+" dialect: Darwazi.
+" dialect: Tehrani.
+" dialect: Dehwari.
+" dialects are mutually intelligible. 
+" grammatical gender: none. 
+" null subject/pro-drop: yes. 
+" case markings: yes. 
+" nouns marked for accusative case (only). #all other oblique cases are marked by prepositions. 
+" auxiliary verbs: yes.
+" verbs reflect: tense: present.
+" verbs reflect: tense: preterite.
+" verbs reflect: tense: imperfect.
+" verbs reflect: tense: future.
+" verbs reflect: tense: perfect.
+" verbs reflect: tense: pluperfect.
+" verbs reflect: mood: indicative.
+" verbs reflect: mood: subjunctive.
+" verbs reflect: aspect: perfective.
+" verbs reflect: aspect: progressive. 
+" verbs reflect: aspect: imperfective.
+" verbs reflect: voice: active.
+" verbs reflect: voice: passive.
+" IPA vowels: æ e o iː uː ɒː .
+" IPA consonants: m n ŋ p b t d k ɡ ɢ ʧ ʤ  f v s z ʃ ʒ x ɣ h ɾ r l j . 
+
+
+bEnglish-> English. 
 :" <Indo-European>.
 :" <Germanic>.
-" is written using the Latin alphabet. 
-" <SVO> word order -g. 
+" is written using the Latin alphabet.  
+" <SVO> word order. 
+
 :Finnish-> Suomi. 
 :" -> Suomen kieli. 
 :" <Uralic>.
@@ -52,11 +307,6 @@
 " is written using the Syriac alphabet.
 " <VSO> word order. 
 " has only three vowels and two dipthongs. 
-:Farsi-> Persian. 
-:" < Indo-European>. 
-:" <Indo-Iranian>.
-" is written using Perso-Arabic script. 
-" <SOV> word order. 
 :Dari-> Dari(Persian). 
 :" <Indo-European>.
 :" <Indo-Iranian>. 
@@ -114,4 +364,4 @@
 :" <Italic>. 
 " no strict word order
 - sentence structure highly flexible
-- <SOV> is most frequent # outside of poetry. 
+
